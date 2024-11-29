fbd2a8a480dabae4e5bb4dc66d187d8f0155f312
RomanPonce <romanponce202@gmail.com>
2024-11-29 18:38:49 -0300

    HOLA

--- app.js
+++ app.js
@@ -1,49 +1,89 @@
 const myLibrery = [];
-const btn = document.querySelector('button');
-const list = document.querySelector('ul');
-const table = document.querySelector('table');
+const btnShow = document.querySelector(".btn-show");
+const bookAdd = document.querySelector(".book-add");
 
+btnShow.addEventListener("click", formShow);
+bookAdd.addEventListener("click", addCardBook);
 
-function Book(title,author,pages){
-    this.title =  title;
-    this.author =  author;
-    this.pages = pages;
-    this.show = () => `Title ${this.title} Author ${this.author} Apginas ${this.pages}`;
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
 }
 
-btn.addEventListener("click",addBookToLibrary);
-
-function addBookToLibrary(){
-    event.preventDefault();
-
-    const title = document.querySelector(".title").value;
-    const author = document.querySelector(".author").value;
-    const pages = document.querySelector(".pages").value;
-    const book = new Book(title,author,pages);
-    myLibrery.push(book);
-    showBookOnTable(book) 
-    
-    document.querySelector(".title").value = "";
-    document.querySelector(".author").value = "";
-    document.querySelector(".pages").value = "";
+function formShow() {
+  event.preventDefault();
+  const form = document.querySelector(".form-books");
+  form.style.display = "flex";
 }
 
-function showBookOnTable(book){
-    const row = document.createElement('tr');
-    const titleRow = document.createElement('td');
-    const authorRow = document.createElement('td');
-    const pagesRow = document.createElement('td');
+function addCardBook() {
+  event.preventDefault();
+  const containerCard = document.querySelector(".container-card");
 
-    titleRow.textContent = book.title;
-    authorRow.textContent = book.author;
-    pagesRow.textContent = book.pages;
-    
-    row.appendChild(titleRow);
-    row.appendChild(authorRow);
-    row.appendChild(pagesRow);
+  const form = document.querySelector(".form-books");
+  const bookTitle = document.querySelector(".book-title").value;
+  const bookAuthor = document.querySelector(".book-author").value;
+  const bookPages = document.querySelector(".book-pages").value;
 
-    table.appendChild(row);
+  const book = new Book(bookTitle, bookAuthor, bookPages);
+
+  myLibrery.push(book);
+
+  const card = document.createElement("div");
+  const title = document.createElement("h3");
+  const author = document.createElement("p");
+  const pages = document.createElement("p");
+
+  const status = document.createElement("select");
+  const read = document.createElement("option");
+  const earring = document.createElement("option");
+  const notStart = document.createElement("option");
+
+  card.classList.add("card");
+
+  title.classList.add("title");
+  author.classList.add("author");
+  pages.classList.add("pages");
+
+  status.classList.add("status");
+  read.classList.add("read");
+  earring.classList.add("earring") ;
+  notStart.classList.add("notStart");
+
+  title.textContent = bookTitle;
+  author.textContent = bookAuthor;
+  pages.textContent = bookPages;
+
+  read.textContent = "read";
+  earring.textContent = "earring";
+  notStart.textContent = "Not start";
+
+  status.appendChild(read);
+  status.appendChild(earring);
+  status.appendChild(notStart);
+
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(status);
+
+  form.addEventListener("submit", formValidation);
+
+  containerCard.appendChild(card);
+  form.style.display = "none";
+  document.querySelector(".book-title").value = "";
+  document.querySelector(".book-author").value = "";
+  document.querySelector(".book-pages").value = "";
 }
 
+function formValidation(){
+  const bookTitle = document.querySelector(".book-title").value;
+  const bookAuthor = document.querySelector(".book-author").value;
+  const bookPages = document.querySelector(".book-pages").value;
 
+  if(bookTitle === "" || bookAuthor === "" || bookPages === ""){
+    form.style.display = "flex";
+  }
 
+}
