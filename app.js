783b3bc3668d444b70465baf95c9ec9a6f1a9b83
RomanPonce <romanponce202@gmail.com>
2024-11-30 19:31:28 -0300

    Borre por completo la funcion que añade libros para hacerla mucho mejor

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const myLibrery = [];
 const btnShow = document.querySelector(".btn-show");
 const bookAdd = document.querySelector(".book-add");
-
-btnShow.addEventListener("click", formShow);
-bookAdd.addEventListener("click", addCardBook);
+const form = document.querySelector("form");
+const dialog = document.querySelector("dialog");
 
 function Book(title, author, pages) {
   this.title = title;
@@ -11,79 +10,16 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
-function formShow() {
-  event.preventDefault();
-  const form = document.querySelector(".form-books");
-  form.style.display = "flex";
+bookAdd.addEventListener("click", addCardBook);
+form.addEventListener("submit", formValidation);
+btnShow.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+function formValidation(event){
 }
 
-function addCardBook() {
-  event.preventDefault();
-  const containerCard = document.querySelector(".container-card");
+function addCardBook(event) {
 
-  const form = document.querySelector(".form-books");
-  const bookTitle = document.querySelector(".book-title").value;
-  const bookAuthor = document.querySelector(".book-author").value;
-  const bookPages = document.querySelector(".book-pages").value;
-
-  const book = new Book(bookTitle, bookAuthor, bookPages);
-
-  myLibrery.push(book);
-
-  const card = document.createElement("div");
-  const title = document.createElement("h3");
-  const author = document.createElement("p");
-  const pages = document.createElement("p");
-
-  const status = document.createElement("select");
-  const read = document.createElement("option");
-  const earring = document.createElement("option");
-  const notStart = document.createElement("option");
-
-  card.classList.add("card");
-
-  title.classList.add("title");
-  author.classList.add("author");
-  pages.classList.add("pages");
-
-  status.classList.add("status");
-  read.classList.add("read");
-  earring.classList.add("earring") ;
-  notStart.classList.add("notStart");
-
-  title.textContent = bookTitle;
-  author.textContent = bookAuthor;
-  pages.textContent = bookPages;
-
-  read.textContent = "read";
-  earring.textContent = "earring";
-  notStart.textContent = "Not start";
-
-  status.appendChild(read);
-  status.appendChild(earring);
-  status.appendChild(notStart);
-
-  card.appendChild(title);
-  card.appendChild(author);
-  card.appendChild(pages);
-  card.appendChild(status);
-
-  form.addEventListener("submit", formValidation);
-
-  containerCard.appendChild(card);
-  form.style.display = "none";
-  document.querySelector(".book-title").value = "";
-  document.querySelector(".book-author").value = "";
-  document.querySelector(".book-pages").value = "";
-}
-
-function formValidation(){
-  const bookTitle = document.querySelector(".book-title").value;
-  const bookAuthor = document.querySelector(".book-author").value;
-  const bookPages = document.querySelector(".book-pages").value;
-
-  if(bookTitle === "" || bookAuthor === "" || bookPages === ""){
-    form.style.display = "flex";
-  }
-
+  
 }
