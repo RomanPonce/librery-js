a2fa6dbc8b456d87383c59a3e4ab67ec09040af9
RomanPonce <romanponce202@gmail.com>
2024-12-01 16:59:40 -0300

    La validacion del form fue impletado. El projecto avanzada desde un mejor punto al separar la variables globales en modulos

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const myLibrery = [];
-const btnShow = document.querySelector(".btn-show");
-const bookAdd = document.querySelector(".book-add");
-const form = document.querySelector("form");
-const dialog = document.querySelector("dialog");
+import {
+  openModal,
+  form,
+  dialog,
+  containerBooks,
+  myLibrery,
+} from "./globals.js";
 
 function Book(title, author, pages) {
   this.title = title;
@@ -10,16 +12,31 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
-bookAdd.addEventListener("click", addCardBook);
-form.addEventListener("submit", formValidation);
-btnShow.addEventListener("click", () => {
+openModal.addEventListener("click", () => {
   dialog.showModal();
 });
 
-function formValidation(event){
+form.addEventListener("submit", getInputsForm);
+
+function getInputsForm(event) {
+  event.preventDefault();
+
+  const titleBook = document.querySelector(".title-book").value;
+  const authorBook = document.querySelector(".author-book").value;
+  const pagesBook = document.querySelector(".pages-book").value;
+  const div = document.createElement("div");
+
+  const book = new Book(titleBook, authorBook, pagesBook);
+  myLibrery.push(book);
+
+  div.innerHTML = `
+              <h3>${titleBook}</h3>
+              <p>${book.author}</p>
+              <p>${book.pages}</p>
+              <button>Eliminar</button>`;
+  containerBooks.appendChild(div);
+
+  dialog.close();
 }
 
-function addCardBook(event) {
-
-  
-}
+console.log("HOLA");
---
+++ globals.js
@@ -0,0 +1,7 @@
+// Variables globales
+
+export const openModal = document.querySelector(".open-modal");
+export const form = document.querySelector("form");
+export const dialog = document.querySelector("dialog");
+export const containerBooks = document.querySelector(".container-books");
+export const myLibrery = [];
